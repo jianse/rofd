c660e951b6fb6fb2ae4b65e72ea6029cd5e56497
liqiang <1484190946@qq.com>
2024-08-02 18:01:11 +0800

    custom fontmgr

--- src/render/font.rs
+++ src/render/font.rs
@@ -1,10 +1,15 @@
-use std::{collections::HashMap, path::PathBuf};
+use std::{
+    collections::{HashMap, HashSet},
+    fs,
+    path::PathBuf,
+};
 
 use skia_safe::{FontStyle, Typeface};
+use walkdir::WalkDir;
 
 struct FontMgr {
     system_font_mgr: skia_safe::FontMgr,
-    font_cache: HashMap<String, Typeface>,
+    font_cache: HashMap<String, FontStyleSet>,
 }
 
 impl FontMgr {
@@ -16,8 +21,58 @@ impl FontMgr {
             font_cache: HashMap::new(),
         }
     }
-    pub fn new_with_font_dir(&self, font_dir: &PathBuf) -> Self {
-        todo!()
+    pub fn new_with_font_dir(font_dir: &PathBuf) -> Self {
+        let sys_fm = skia_safe::FontMgr::new();
+        let mut font_cache = HashMap::new();
+        // log::debug!("init font manager,{}",font_dir.);
+        for entry in WalkDir::new(font_dir)
+            .into_iter()
+            .filter_map(|e| e.ok())
+            .filter(|e| {
+                e.file_name()
+                    .to_str()
+                    .map(|f| f.to_ascii_lowercase().ends_with(".ttf"))
+                    .unwrap_or(false)
+                    && e.file_type().is_file()
+            })
+        {
+            log::debug!("try load font \"{}\"", entry.path().display());
+            let path = entry.path();
+            let bytes = fs::read(path);
+            if let Ok(bytes) = bytes {
+                let ff = sys_fm.new_from_data(bytes.as_ref(), 0);
+                if let Some(ff) = ff {
+                    let family_names = ff
+                        .new_family_name_iterator()
+                        .map(|l| l.string)
+                        .collect::<HashSet<String>>();
+                    for family_name in family_names {
+                        log::debug!(
+                            "font family name \"{}\", font_style {:?}",
+                            family_name,
+                            ff.font_style()
+                        );
+                        font_cache
+                            .entry(family_name.clone())
+                            .or_insert(FontStyleSet::new());
+                        font_cache
+                            .entry(family_name)
+                            .and_modify(|e: &mut FontStyleSet| {
+                                e.add_font(ff.clone());
+                            });
+                    }
+                } else {
+                    log::warn!("warn! load font error. path {}", path.display());
+                }
+            } else {
+                log::warn!("warn! read file error. path {}", path.display());
+            }
+        }
+
+        Self {
+            system_font_mgr: sys_fm,
+            font_cache,
+        }
     }
     pub fn match_family_style(
         &self,
@@ -34,6 +89,21 @@ struct FontStyleSet {
 }
 
 impl FontStyleSet {
+    fn new() -> Self {
+        Self { fonts: vec![] }
+    }
+    fn add_font(&mut self, font: Typeface) {
+        let dup = self.match_style(font.font_style());
+        if dup.is_some() {
+            log::warn!(
+                "warn! font dup. family_name={},style={:?}",
+                font.family_name(),
+                font.font_style()
+            );
+            return;
+        }
+        self.fonts.push(font);
+    }
     pub fn count(&self) -> usize {
         self.fonts.len()
     }
@@ -57,7 +127,24 @@ impl FontStyleSet {
 }
 #[cfg(test)]
 mod tests {
+    use std::env;
+
     use skia_safe::Typeface;
+
+    use super::FontMgr;
+
+    fn init_logger() {
+        let e = env_logger::builder()
+            // Include all events in tests
+            .filter_level(log::LevelFilter::max())
+            // Ensure events are captured by `cargo test`
+            .is_test(true)
+            // Ignore errors initializing the logger if tests race to configure it
+            .try_init();
+        if e.is_err() {
+            println!("warn! init logger error");
+        }
+    }
 
     #[test]
     fn test_typeface_eq() {
@@ -72,5 +159,16 @@ mod tests {
         let cur_kai = cur_kai.unwrap();
         cur_kai.family_name();
         assert!(!Typeface::equal(sys_kai, cur_kai));
+    }
+
+    #[test]
+    fn test_new_with_font_dir() {
+        init_logger();
+        let mut cur_dir = env::current_dir().unwrap();
+        cur_dir.push("fonts");
+        println!("--{}--", cur_dir.display());
+        let fm = FontMgr::new_with_font_dir(&cur_dir);
+        let font_family_count = fm.font_cache.len();
+        dbg!(font_family_count);
     }
 }
